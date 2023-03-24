11b180df2904f38bb51aaf8d2aa8c54f35884959
Maxim <exclusivejester@gmail.com>
2023-03-24 19:18:33 +0500

    install commander.js, add gendiff.js, add content

---
+++ bin/gendiff.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander/esm.mjs'
+
+const program = new Command();
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-h, --help', 'display help for command')
+  .parse(process.argv);
+
+if (program.help) {
+  program.help();
+}
