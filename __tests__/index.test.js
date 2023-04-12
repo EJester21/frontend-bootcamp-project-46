03e52dc9d4c88ca3a08c41fc01994b75eb9d3e28
Maxim <exclusivejester@gmail.com>
2023-04-12 19:41:02 +0500

    added a test for json

---
+++ __tests__/index.test.js
@@ -0,0 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
+import gendiff from '../src/index.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('compares flat JSON files correctly', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  const expected = readFileSync(getFixturePath('result_json.txt'), 'utf-8').trim();
+
+  expect(gendiff(filepath1, filepath2)).toEqual(expected);
+});
